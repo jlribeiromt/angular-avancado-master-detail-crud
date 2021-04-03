af0dc02d79a70d65aee8132ddb0ad023b810df49
jlribeiromt <jl.dr@hotmail.com>
2021-04-03 16:14:06 -0400

    Adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,10 @@
-export class Category {
+import { BaseResourceModel } from '../../../shared/models/base-resources.model';
+export class Category extends BaseResourceModel {
   constructor(
     public id?: number,
     public name?: string,
     public description?: string
-  ) { }
+  ) {
+    super();
+  }
 }
