ddea774a1c71515200226b2575b49b4b3e5272ff
jlribeiromt <jl.dr@hotmail.com>
2021-04-12 19:19:07 -0400

    Refactoring EntryFormComponent

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -1,31 +1,18 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { EntryService } from '../shared/entry.service';
-import { switchMap } from 'rxjs/operators';
-import toastr from 'toastr';
 import { Entry } from '../shared/entry.model';
 import { PrimeNGConfig } from 'primeng/api/primengconfig';
 import { Category } from '../../categories/shared/category.model';
 import { CategoryService } from '../../categories/shared/category.service';
+import { BaseResourceFormComponent } from '../../../shared/components/base-resource-form/base-resource-form.component';
 
 @Component({
   selector: 'app-entry-form',
   templateUrl: './entry-form.component.html',
   styleUrls: ['./entry-form.component.css'],
 })
-export class EntryFormComponent implements OnInit, AfterContentChecked {
-  currentAction: string;
-  entryForm: FormGroup;
-  pageTitle: string;
-  serverErrorMessages: string[] = null;
-  submittingForm: boolean = false;
-  entry: Entry = new Entry();
+export class EntryFormComponent extends BaseResourceFormComponent<Entry> implements OnInit {
   categories: Array<Category>;
 
   imaskConfig = {
@@ -83,23 +70,16 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   };
 
   constructor(
-    private entryService: EntryService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private categoryServe: CategoryService
-  ) {}
-
-  ngOnInit(): void {
-    this.setCurrentAction();
-    this.buildEntry();
-    this.loadEntry();
-    this.loadCategories();
-    console.log(this.typeOptions);
+    protected entryService: EntryService,
+    protected categoryServe: CategoryService,
+    protected injector: Injector
+  ) {
+    super(injector, new Entry(), entryService, Entry.fromJson)
   }
 
-  ngAfterContentChecked(): void {
-    this.setPageTitle();
+  ngOnInit(): void {
+    this.loadCategories();
+    super.ngOnInit();
   }
 
   get typeOptions(): Array<any> {
@@ -111,33 +91,8 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     });
   }
 
-  submitForm(): void {
-    this.submittingForm = true;
-
-    if (this.currentAction == 'new') {
-      this.createEntry();
-    } else {
-      // currentAction = edit
-      this.updateEntry();
-    }
-  }
-
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.entryForm.controls;
-  }
-
-  // PRIVATE METHODS
-  private setCurrentAction(): void {
-    if (this.route.snapshot.url[0].path == 'new') {
-      this.currentAction = 'new';
-    } else {
-      this.currentAction = 'edit';
-    }
-  }
-
-  private buildEntry(): void {
-    this.entryForm = this.formBuilder.group({
+  private buildResourceForm(): void {
+    this.resourceForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
@@ -149,74 +104,18 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     });
   }
 
-  private loadEntry(): void {
-    if (this.currentAction == 'edit') {
-      this.route.paramMap
-        .pipe(
-          switchMap((params) => this.entryService.getById(+params.get('id')))
-        )
-        .subscribe(
-          (entry) => {
-            this.entry = entry;
-            this.entryForm.patchValue(entry); // binds loaded entry data to EntryForm
-          },
-          (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
-        );
-    }
-  }
-
   private loadCategories() {
     this.categoryServe
       .getAll()
       .subscribe((categories) => (this.categories = categories));
   }
 
-  private setPageTitle(): void {
-    if (this.currentAction == 'new') {
-      this.pageTitle = 'Cadastro de Novo Lançamento';
-    } else {
-      const entryName = this.entry.name || '';
-      this.pageTitle = 'Editando Lançamento: ' + entryName;
-    }
+  protected creationPageTitle(): string {
+    return "Cadastro de Novo Lançamento";
   }
 
-  private createEntry(): void {
-    const entry = Entry.fromJson(this.entryForm.value);
-
-    this.entryService.create(entry).subscribe(
-      (entry) => this.actionsForSuccess(entry),
-      (error) => this.actionForError(error)
-    );
-  }
-
-  private updateEntry(): void {
-    const entry = Entry.fromJson(this.entryForm.value);
-
-    this.entryService.update(entry).subscribe(
-      (entry) => this.actionsForSuccess(entry),
-      (error) => this.actionForError(error)
-    );
-  }
-
-  private actionsForSuccess(entry: Entry): void {
-    toastr.success('Solicitação processada com sucesso!');
-
-    // redirect/reload component page
-    this.router
-      .navigateByUrl('entries', { skipLocationChange: true })
-      .then(() => this.router.navigate(['entries', entry.id, 'edit']));
-  }
-
-  private actionForError(error): void {
-    toastr.error('Ocorreu um erro ao processar a sua solicitação.!');
-    this.submittingForm = false;
-
-    if (error.status === 422) {
-      this.serverErrorMessages = JSON.parse(error._body).errors;
-    } else {
-      this.serverErrorMessages = [
-        'Falha na comunicação com servidor. Por favor tente mais tarde.',
-      ];
-    }
+  protected editionPageTitle(): string {
+    const resourceName = this.resource.name || "";
+    return "Editando Lançamento: " + resourceName;
   }
 }
